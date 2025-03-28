fb9189c7f3de6397a0ef4556d97eb522bc5fecd2
itsPixel <brandonllanes16@gmail.com>
2025-03-28 13:26:41 -0700

    add option to read in from rabbitmq

--- Reservoir_Challenge/app.py
+++ Reservoir_Challenge/app.py
@@ -5,14 +5,20 @@ Created on Wed Mar 28 12:48:00 2025
 @author: bllanes
 """
 
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 from pydantic import BaseModel
 
 # we may want to use another RabbitMQ connection to retrieve the data from data cleaning
+"""
+import pika
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+channel.queue_declare(queue='cleaned_data_queue', durable=True)
+"""
 
 app = FastAPI(
-    title="Data Cleaning API",
-    description="Post/Retrieve",
+    title="Data Retrieval API",
+    description="After data has been cleaned, this API is used to transfer the data to frontend",
     version="0.0.1")
 
 # Define request model (we may need to modify this if we want to get from another queue)
@@ -27,3 +33,22 @@ def read_root():
 @app.post("/process_data/")
 def process_data(data: InputData):
     return {"message": f"Hello {data.name}, you are {data.age} years old."}
+
+"""
+@app.get("/get_cleaned_data/")
+def get_cleaned_data():
+    # Fetch the latest cleaned data from RabbitMQ
+    body = channel.basic_get(queue="cleaned_data_queue", auto_ack=True)
+    if body:
+        return {"cleaned_data": body.decode()}
+    return {"cleaned_data": None}
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    # WebSocket endpoint for real-time updates.
+    await websocket.accept()
+    while True:
+        body = channel.basic_get(queue="cleaned_data_queue", auto_ack=True)
+        if body:
+            await websocket.send_text(body.decode())
+"""
