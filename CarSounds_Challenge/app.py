55ab3def55e9fa8182ef272bcefb7b32ced17f57
itsPixel <brandonllanes16@gmail.com>
2025-03-28 18:48:09 -0700

    update API to use websocket

--- CarSounds_Challenge/app.py
+++ CarSounds_Challenge/app.py
@@ -5,13 +5,26 @@ Created on Wed Mar 28 12:48:00 2025
 @author: bllanes
 """
 
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Data Retrieval API",
     description="After data has been cleaned, this API is used to transfer the data to frontend",
     version="0.0.1")
+
+origins = [
+    "http://localhost:3000", # React frontend
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allow specific origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 # Audio result to save
 class AudioResult(BaseModel):
@@ -28,15 +41,44 @@ class SimpleCache:
     
 cache = SimpleCache()
 
+# store active websocket connections
+active_connections = []
+
 @app.get("/")
 def read_root():
     return {"message": "Welcome Class CMPE 273 - FastAPI is running!"}
 
+"""
 @app.post("/process_data/")
 def insert_data_in_cache(data: AudioResult):
     cache.set("stored_audio_result", data)
     return {"message": f"Done! cache insert..."}
+"""
 
+@app.post("/process_data/")
+async def insert_data_in_cache(data: AudioResult):
+    cache.set("stored_audio_result", data)
+
+    # broadcast data to all websocket clients
+    for connection in active_connections:
+        await connection.send_json(data.dict())
+
+
+    return {"message": f"Done! cache insert..."}
+
+"""
 @app.get("/retrieve_data/")
 def read_from_cache():
     return cache.get("stored_audio_result")
+"""
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    active_connections.append(websocket)
+
+    try:
+        while True:
+            await websocket.receive_text()
+    except WebSocketDisconnect:
+        active_connections.remove(websocket)
