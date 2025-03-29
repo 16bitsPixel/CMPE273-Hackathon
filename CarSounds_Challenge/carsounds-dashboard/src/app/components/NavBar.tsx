0d60f1c4eb96a56a34a9c47c7d1ad7d24be9e812
itsPixel <brandonllanes16@gmail.com>
2025-03-28 18:47:28 -0700

    use websocket to get data

---
+++ CarSounds_Challenge/carsounds-dashboard/src/app/components/NavBar.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import { styled } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+
+export default function Header() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="sticky" sx={{ backgroundColor: '046B99', color: 'white' }} >
+        <Toolbar sx={{ justifyContent: 'space-between' }}>
+          {/* Left side logo */}
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ display: { xs: 'none', md: 'block' } }}
+          >
+            Car Sounds Dashboard
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
